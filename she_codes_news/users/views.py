11b34c850fb0d7e9c418f3a1f1890b48b5eeae3c
Miao Zhao <miao_zhao@yahoo.com>
2022-06-04 11:31:33 +1000

    Refactor users/views

--- she_codes_news/users/views.py
+++ she_codes_news/users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+# reverse_lazy: change django to a url
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.views import generic
+from .models import CustomUser
+from .forms import CustomUserCreationForm
 
 # Create your views here.
+# login and logout's views are predefined in django
+class CreateAccountView(CreateView):
+    form_class = CustomUserCreationForm
+    # reverse_lazy lookup the url path from the saved name
+    success_url = reverse_lazy("login")
+    template_name = "users/createAccount.html"
