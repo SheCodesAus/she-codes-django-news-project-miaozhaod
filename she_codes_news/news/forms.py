75abdfb4b074a462a347c8e98d2cf673f2dd1edd
Miao Zhao <miao_zhao@yahoo.com>
2022-06-04 12:12:01 +1000

    Delete author from news/forms

--- she_codes_news/news/forms.py
+++ she_codes_news/news/forms.py
@@ -6,7 +6,7 @@ from .models import NewsStory
 class StoryForm(ModelForm):
     class Meta:
         model = NewsStory
-        fields = ["title", "author", "pub_date", "content", "img"]
+        fields = ["title", "pub_date", "content", "img"]
         widgets = {
             "pub_date": forms.DateInput(
                 format=("%m/%d/%Y"), attrs={"class": "form-control", "placeholder": "Select a date", "type": "date"}
