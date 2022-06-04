d0ef225fdb65f9d341fadcf4126dc693a77d802f
Miao Zhao <miao_zhao@yahoo.com>
2022-06-04 12:38:50 +1000

    Update news.views to associate author

--- she_codes_news/news/views.py
+++ she_codes_news/news/views.py
@@ -3,27 +3,37 @@ from .models import NewsStory
 from django.urls import reverse_lazy
 from .forms import StoryForm
 
+
 class IndexView(generic.ListView):
-    template_name = 'news/index.html'
+    template_name = "news/index.html"
 
     def get_queryset(self):
-        '''Return all news stories.'''
+        """Return all news stories."""
         return NewsStory.objects.all()
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['latest_stories'] = NewsStory.objects.all()[:4]
-        context['all_stories'] = NewsStory.objects.all()
-        context['stories_by_date'] = NewsStory.objects.order_by('pub_date')
+        context["latest_stories"] = NewsStory.objects.all()[:4]
+        context["all_stories"] = NewsStory.objects.all()
+        context["stories_by_date"] = NewsStory.objects.order_by("pub_date")
         return context
+
 
 class StoryView(generic.DetailView):
     model = NewsStory
-    template_name = 'news/story.html'
-    context_object_name = 'story'
+    template_name = "news/story.html"
+    context_object_name = "story"
+
 
 class AddStoryView(generic.CreateView):
     form_class = StoryForm
-    context_object_name = 'storyForm'
-    template_name = 'news/createStory.html'
-    success_url = reverse_lazy('news:index')
+    context_object_name = "storyForm"
+    template_name = "news/createStory.html"
+    success_url = reverse_lazy("news:index")
+
+    # overriding form_valid which is on generic.CreateView
+    def form_valid(self, form):
+        # to set author to the  user logged in
+        form.instance.author = self.request.user
+        # call the form_valid on generic.CreateVie
+        return super().form_valid(form)
