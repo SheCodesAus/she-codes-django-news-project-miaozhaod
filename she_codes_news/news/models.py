915ad71d56ffa9ca95cf48a1b6576a7629517d45
Miao Zhao <miao_zhao@yahoo.com>
2022-06-04 12:02:11 +1000

    Update news/models to get author data via the foreign key

--- she_codes_news/news/models.py
+++ she_codes_news/news/models.py
@@ -1,9 +1,12 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
 class NewsStory(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
+    # author = models.CharField(max_length=200), 
+    # on_delete means if you delete a user, it means everything associated with that user will be deleted
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     pub_date = models.DateTimeField()
     content = models.TextField()
-    img = models.CharField(max_length=500, default='https://picsum.photos/600')
+    img = models.CharField(max_length=500, default="https://picsum.photos/600")
