dc38272a5229884b4c96e22d10aeadda0a585bf7
Miao Zhao <miao_zhao@yahoo.com>
2022-06-07 19:44:43 +1000

    Create url for `view-account`

--- she_codes_news/users/urls.py
+++ she_codes_news/users/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import CreateAccountView
+from .views import CreateAccountView, ViewAccountView
 
 app_name = "users"
 
-urlpatterns = [path("create-account/", CreateAccountView.as_view(), name="createAccount")]
+urlpatterns = [
+    path("create-account/", CreateAccountView.as_view(), name="createAccount"),
+    path("view-account/<int:pk>/", ViewAccountView.as_view(), name="viewAccount"),
+]
