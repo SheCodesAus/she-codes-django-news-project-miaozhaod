8891898bb651d5b4db239f5d905502627e1fa571
Miao Zhao <miao_zhao@yahoo.com>
2022-06-11 10:02:19 +1000

    Add whitenoise to the middleware

--- she_codes_news/she_codes_news/settings.py
+++ she_codes_news/she_codes_news/settings.py
@@ -49,6 +49,7 @@ LOGOUT_REDIRECT_URL = "news:index"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
