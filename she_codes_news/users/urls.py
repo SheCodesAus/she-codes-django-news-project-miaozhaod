73e1b7d5311311f53a7142e358a72743d435bdbe
Miao Zhao <miao_zhao@yahoo.com>
2022-06-18 22:27:35 +1000

    Update by adding on path of `allUsers` as index path for the users app

--- she_codes_news/users/urls.py
+++ she_codes_news/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CreateAccountView, ViewAccountView
+from .views import CreateAccountView, ViewAccountView, AllUsersView
 
 app_name = "users"
 
 urlpatterns = [
     path("create-account/", CreateAccountView.as_view(), name="createAccount"),
     path("view-account/<int:pk>/", ViewAccountView.as_view(), name="viewAccount"),
+    path("", AllUsersView.as_view(), name="allUsers"),
 ]
